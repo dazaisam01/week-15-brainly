7cf3f515f5f68e8c86c563e56aff54e3712acf46
samarthmore078 <samarthmore369@gmail.com>
2025-01-15 18:20:33 +0530

    Init

---
+++ src/db.ts
@@ -0,0 +1,2 @@
+//In this file we will create all DB logic
+//Create modules and schemas here
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import express from "express";
+import mongoose from "mongoose";
+import { Jwt } from "jsonwebtoken";
+
+
+const app = express();
